feebcb7bd6fdf6ec4ee6d058f240c6aaa6fb2e11
Reuben Uzor <reubenuzor@gmail.com>
2022-09-11 22:17:54 -0700

    Coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entrypoint
+ * 
+ * Return = (0)
+ */
+int main(void)
+{
+/**
+ * Inserts string
+ */
+	char str[] = "Programming is like building a multilingual puzzle"
+
+/**
+ * sending output
+ */
+		puts(str);
+	return 0;
+}
