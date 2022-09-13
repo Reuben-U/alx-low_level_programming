3fbf2d4b4d5adcf8bad7e11b7ff66330db48c3dd
Reuben Uzor <reubenuzor@gmail.com>
2022-09-12 20:10:12 -0700

    improved1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/** main - Entrypoint
- * 
- * Return = (0)
+/**
+ *main - Entry point
+ *
+ *Return: 0
  */
 int main(void)
 {
-/**
- * sending output
- */
-		puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
