0b1c69824ad6296cac183eb057129cc924a914b1
Reuben Uzor <reubenuzor@gmail.com>
2022-09-12 21:44:44 -0700

    UPDATES1234

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 /**
  * sizeof displays character sizes
  */
-	printf("size of a char: %zu byte(s)\n", sizeof(charT));
-	printf("size of an int: %zu byte(s)\n", sizeof(intT));
-	printf("size of a long int: %zu byte(s)\n", sizeof(longT));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(longlongintT));
-	printf("size of a float: %zu byte(s)\n", sizeof(floatT));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charT));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intT));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longT));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintT));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatT));
 return (0);
 }
