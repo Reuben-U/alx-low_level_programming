2edefc45487b5e8c2530b499c0af3a306ed54961
Reuben Uzor <reubenuzor@gmail.com>
2022-09-13 22:14:25 -0700

    All good but betty

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ *Description: code tests if statements
+ *
+ * return: 0 'Always success';
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if n > 0;
+	{
+		printf("%d is positive\n", &n);
+	}
+	else if n == 0;
+	{
+		printf("%d is zero\n", &n);
+	}
+	else
+	{
+		printf("%d is negative\n", &n);
+	}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+We begin using if and while statements
