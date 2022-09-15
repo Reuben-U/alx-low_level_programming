fb93b97eff884a6a1985bbc2d06ac3c4f8b84dee
Reuben Uzor <reubenuzor@gmail.com>
2022-09-14 21:34:41 -0700

    prints alphabet12

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 	
-	for (j == 1, j <= 10; j++)
+	for (j = '1'; j <= '10'; j++)
 	{
-		for (i ='a', i <= 'z'; i++)
+		for (i ='a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
