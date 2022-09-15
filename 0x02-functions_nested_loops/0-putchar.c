cd04c76870ebd7cb3f28d475538b46d043252dde
Reuben Uzor <reubenuzor@gmail.com>
2022-09-14 20:52:13 -0700

    done incr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  */
 int _putchar(char c)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	return (write(1, &c, 1));
 }
