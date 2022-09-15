fb381fd12030346015195347040825bb265eb553
Reuben Uzor <reubenuzor@gmail.com>
2022-09-14 19:45:36 -0700

    Addd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/**
+ *_ putchar.c - displays _putchar
+ * @putchar: The characters to be printed
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and erno is set appropriately.
+ */
+int _putchar(char c)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	return (write(1, &c, 1));
+}
