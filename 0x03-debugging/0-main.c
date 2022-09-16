ca41c62a65c7b5ebf1369e57b128b3caf72eb20e
Reuben Uzor <reubenuzor@gmail.com>
2022-09-15 21:57:59 -0700

    nor

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * 0-main.c - Tests positive_or_negative() gives correct output when i=0
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
