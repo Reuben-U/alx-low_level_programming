64811780149c351fcb659503ad3a6806eae834ed
Reuben Uzor <reubenuzor@gmail.com>
2022-09-19 21:57:42 -0700

    introduced

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - converts input to 98
+ *
+ * Return: Always 0.
+ */
+void rest_to_98(int *n)
+{
+	int i;
+	int *n;
+	
+	n = &i
+	n = 98
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Reset_to_98 reassigns variable to 98
+ *
+ * Returns 0
+ */
+void reset_to_98(int *n);
+
+#endif
