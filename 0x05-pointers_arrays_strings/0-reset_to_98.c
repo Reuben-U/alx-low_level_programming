5557177345b12eb2a69c48ab38b40dd935f9ff0a
Reuben Uzor <reubenuzor@gmail.com>
2022-09-20 20:06:50 -0700

    all betty issues corrected

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - converts input to 98
+ * reset_to_98 - converts input to 98
+ *
+ *@n: pointer for integer
  *
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * Reset_to_98 reassigns variable to 98
- *
- * Returns 0
- */
 void reset_to_98(int *n);
 
 #endif
