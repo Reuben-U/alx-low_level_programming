c0a6bbdf9b3f02c3c4596b10003e6fbf267a8fd6
Reuben Uzor <reubenuzor@gmail.com>
2022-09-23 18:10:16 -0700

    new addition1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets but skips some
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <=; ch++)
+	{
+	if (ch == 'e')
+	ch++;
+	if (ch == 'q')
+	ch++;
+	putchar(ch);
+	}
+	putchar('\n');
+	
+	return (0);
+}
