1208129b3da0a07323580de77751e97c24cfdea4
Reuben Uzor <reubenuzor@gmail.com>
2022-09-26 04:26:50 -0700

    updated putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+int _putchar(char c);
 /**
  * 0-putchar.c - displays _putchar
  *
@@ -7,9 +8,15 @@
  * Return: on success 1.
  * on error, -1 is returned, and erno is set appropriately.
  */
-int _putchar(void)
+int main(void)
 {
 	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');                                                         		_putchar('h');
+        _putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return (1);
+return (0);
 }
