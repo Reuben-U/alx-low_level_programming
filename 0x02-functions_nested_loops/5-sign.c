618dbd4e7ebb63826ae13bf22060399df49c12de
Reuben Uzor <reubenuzor@gmail.com>
2022-09-26 13:03:59 -0700

    Edited 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of output
+ * @n: number to be checked
+ * Return: 0 if 0
+ * 1 if +ve
+ * -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	_putchar('1');
+	return (-1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 Nested loops
 Added lower case detection file
 _isalpha checks if alphabet is lower case
+5-sign added to show signs of integers
