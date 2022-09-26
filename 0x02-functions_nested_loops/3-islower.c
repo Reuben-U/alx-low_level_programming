45a3ebff9aa829d86e1ed8c921d7bdbbb5a4a73f
Reuben Uzor <reubenuzor@gmail.com>
2022-09-26 06:12:04 -0700

    improved betty error corrected

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
-/** _islower - checks lower case
- * Return - 1 if lower case or 0 otherwise.
+/**
+ **_islower - checks lower case
+ *@c: alphabet to be checked.
+ *Return: 1 if lower case or 0 otherwise.
  */
 
 int _islower(int c)
 {
-	if (c>=97 && c<=122)
+	if (c >= 97 && c <= 122)
 	{
 	return (1);
 	}
-return(0);
+return (0);
 }
