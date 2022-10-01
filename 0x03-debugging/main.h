1bc32d73d826beec7e5f093468d5423c20502ecb
Reuben Uzor <reubenuzor@gmail.com>
2022-10-01 14:21:13 -0700

    updated file1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(void);
-int largest_number(int a, int b, int c);
+void positive_or_negative(void)
+int largest_number(int a, int b, int c)
 
 #endif
 /**
