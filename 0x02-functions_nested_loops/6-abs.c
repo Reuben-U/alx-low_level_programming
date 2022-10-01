14a6c0901c924001839794b4bc70aeb7369b1f42
Reuben Uzor <reubenuzor@gmail.com>
2022-09-30 19:30:47 -0700

    updated abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of int
+ * @a: int to be checked and abs computed
+ * Return: a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	a = a * -1;
+	}
+return (a);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,4 @@ Nested loops
 Added lower case detection file
 _isalpha checks if alphabet is lower case
 5-sign added to show signs of integers
+6 - absolute value funct
