6862a91760e8521db16ad24f9f2478a5bb2dcd65
Reuben Uzor <reubenuzor@gmail.com>
2022-10-02 04:06:18 -0700

    printed numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar((i%10) + '0');
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
