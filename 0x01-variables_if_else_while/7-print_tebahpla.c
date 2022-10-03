804e5094e277ec61870ce07d931332e4c82084e7
Reuben Uzor <reubenuzor@gmail.com>
2022-10-02 22:03:51 -0700

    updated 7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints alphabets in reverse
+*Return: 0.
+*/
+int main(void)
+{
+	int i;
+
+	i = 'z'
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+}
+
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 We begin using if and while statements
 Added alphabet printing loops
 Added numbers
+7-print_tebahpla.c prints in reverse
