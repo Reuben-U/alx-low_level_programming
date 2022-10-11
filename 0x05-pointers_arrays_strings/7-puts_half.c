8a77471993c1f2630afd501275b5b4716ecc85a5
Reuben Uzor <reubenuzor@gmail.com>
2022-10-10 20:19:25 -0700

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,9 +19,9 @@ void puts_half(char *str)
 
 	if (len % 2 != 0)
 	{
-		for (i = (len - 2) / 2; i <= len - 1; i++)
+		for (i = len - 1 / 2; i <= len - 2; i++)
 		{
-			_putchar(str[i]);
+			_putchar(str[i + 1]);
 		}
 	}
 	else
