648237fde2f7b1c70cfdbb0199c889730d0bb794
Reuben Uzor <reubenuzor@gmail.com>
2022-10-10 20:03:12 -0700

    updated add file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: string to be printed
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int i, len = 0;
+	char *pstr;
+
+	pstr = str;
+	for (i = 0; *pstr != '\0'; i++)
+	{
+		len++;
+		pstr++;
+	}
+
+	if (len % 2 != 0)
+	{
+		for (i = (len - 1) / 2; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = len / 2; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ Swap file added
 4-print_rev reverses strings
 print_rev reverses strings
 puts2 prints every other character
+puts_half prints half a string
