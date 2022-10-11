997f4609d258f848cb4a7a9f1bd029aa0e8b7816
Reuben Uzor <reubenuzor@gmail.com>
2022-10-10 18:30:26 -0700

    printrev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *rev_string- reverses strings
+ *@s: string to be reversed
+ *@i++: string increment.
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int i, len = 0;
+	char *ps, b;
+
+	ps = s;
+	for (i = 0; *ps != '\0'; i++)
+	{
+		len++;
+		ps++;
+	}
+	ps--;
+
+
+	for (i = 0; i <= len / 2; i++)
+	{
+		b = *(s + i);
+		*(s + i) = *(ps - i);
+		*(s + len - i) = b;
+	}
+}
