948fc63a5418fabd59870293bb08bc8b6309980d
Reuben Uzor <reubenuzor@gmail.com>
2022-10-13 12:38:31 -0700

    added file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints integers
+ * @array: array to print
+ * @size: size of int array
+ * @action: pointer to be used.
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+		size_t i;
+
+	for ( i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,2 @@
 a: print_name: function prints name via pointers
+array iterator function pointer selects diff print fncs
