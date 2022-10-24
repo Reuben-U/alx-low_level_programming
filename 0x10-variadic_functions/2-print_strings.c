9222d29267d61ba9fd086b4df6f8a6ce6699c4d4
Reuben Uzor <reubenuzor@gmail.com>
2022-10-24 10:20:59 -0700

    updated print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints variadic strings
+ * @separator: string seprator
+ * @n: strings to be printed
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *str;
+	va_list spec;
+
+	va_start(spec, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		str = va_arg(spec, char *);
+		printf("%s", str);
+
+		if (separator == NULL)
+			printf("%s", "nil");
+		else
+			printf("%s", separator);
+	}
+	str = va_arg(spec, char *);
+	printf("%s", str);
+
+	va_end(spec);
+	putchar('\n');
+
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,3 @@
 First Variadic function prints sums of integers entered
 1-print_numbers prints values with separator
+print_strings prints variadic strings
