1e6598d4ff561db7419ca5c8a6d2120987e016fe
Reuben Uzor <reubenuzor@gmail.com>
2022-10-24 01:01:52 -0700

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,10 +2,26 @@
 
 /**
  * print_numbers - prints numbers
+ * @separator: pointer to separator string
+ * @n: integer to be printed
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
+	va_list specially;
+
 	if (separator == NULL)
-		separator == "";
+		return;
+
+	va_start(specially, n);
+		for (i = 0; i < (n - 1); i++)
+		{
+			printf("%d", (va_arg(specially, int)));
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(specially, int));
+	va_end(specially);
+
+	putchar('\n');
 }
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,2 @@
 First Variadic function prints sums of integers entered
+1-print_numbers prints values with separator
